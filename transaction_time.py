d1803982d080d0e9c9d8086a0fefce733e807995
Sarat <saratpavan@gmail.com>
2021-05-29 19:26:26 +0530

    Updated working version

--- transaction_time.py
+++ transaction_time.py
@@ -10,7 +10,7 @@ def time_diff():
     a = df['time'].tolist()
     sc_hlf = datetime.strptime(a[2], '%H:%M:%S') 
     sc_hlf2 = datetime.strptime(a[3], '%H:%M:%S')  
-    sc_res = sc_hlf2 - sc_hlf
+    sc_res = (sc_hlf2 - sc_hlf)/2
     print('The average time between two transactions is :')
     print(sc_res)
 
